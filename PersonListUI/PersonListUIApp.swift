d8af4ed0e73cc656519fbb80e11563700a273196
Artem Bazhanov <abazhanov@yahoo.com>
2021-06-14 22:37:56 +0300

    Initial Commit

---
+++ PersonListUI/PersonListUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonListUIApp.swift
+//  PersonListUI
+//
+//  Created by Artem Bazhanov on 14.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct PersonListUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
